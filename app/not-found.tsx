3beb925f6046a9eabd003b3d3a50eb41b87c57e9
mavgrik <79848516+mavgrik@users.noreply.github.com>
2026-09-05 16:30:41 +0200

    Migliora la pagina Not Found con un titolo e uno stile aggiornati; aggiungi nuove immagini per il sito

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,9 +6,13 @@ import { Button } from "@/components/ui/button";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-start space-y-4 text-center">
-      <h2>l&apos;aquila ha perso la strada</h2>
-      <Image src="/miss.png" alt="404" width={512} height={512} />
-      <p>Controlla il link e riprova</p>
+      <h1 className="text-2xl font-black tracking-tighter uppercase italic md:text-4xl">
+        L&apos;aquila ha perso la strada
+      </h1>
+      <Image src="/miss.png" className="rounded-xl" alt="404" width={512} height={512} />
+      <p className="mx-auto max-w-2xl font-mono text-sm text-muted-foreground md:text-base">
+        Controlla il link e riprova
+      </p>
       <Link href="/">
         <Button variant="outline">
           <Home />
